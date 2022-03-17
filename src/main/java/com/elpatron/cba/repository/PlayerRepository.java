6efc756e4bc2fcee869c95cc2f4b089fb5610dd6
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2022-03-17 14:40:40 +0100

    code refactor

--- src/main/java/com/elpatron/cba/repository/PlayerRepository.java
+++ src/main/java/com/elpatron/cba/repository/PlayerRepository.java
@@ -2,13 +2,8 @@ package com.elpatron.cba.repository;
 
 import com.elpatron.cba.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
-    @Query("select s from Player s where s.number = ?1")
-    Optional<Player> findPlayerByNumber(Integer number);
 }
