c37928020f08c55ede85d64390fb4054e6dffe3f
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2022-03-24 14:52:00 +0100

    allow communication between backend & frontend

--- src/main/java/com/elpatron/cba/CbaApplication.java
+++ src/main/java/com/elpatron/cba/CbaApplication.java
@@ -1,7 +1,16 @@
 package com.elpatron.cba;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CbaApplication {
@@ -10,4 +19,13 @@ public class CbaApplication {
         SpringApplication.run(CbaApplication.class, args);
     }
 
+    @Configuration
+    public class WebConfig extends WebMvcConfigurerAdapter {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+        }
+    }
 }
