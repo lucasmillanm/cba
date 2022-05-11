242b39c996d4543e735caf2c9b76e89143ffc1c0
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2022-05-11 09:01:53 +0200

    remove unused imports

--- src/main/java/com/elpatron/cba/CbaApplication.java
+++ src/main/java/com/elpatron/cba/CbaApplication.java
@@ -1,17 +1,10 @@
 package com.elpatron.cba;
 
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class CbaApplication {
