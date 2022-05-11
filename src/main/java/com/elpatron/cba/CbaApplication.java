f6419f4cd8cf9270beaa27e438f5fe7be6c0a807
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2022-05-11 08:55:08 +0200

    fix backend

--- src/main/java/com/elpatron/cba/CbaApplication.java
+++ src/main/java/com/elpatron/cba/CbaApplication.java
@@ -3,6 +3,7 @@ package com.elpatron.cba;
 import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
