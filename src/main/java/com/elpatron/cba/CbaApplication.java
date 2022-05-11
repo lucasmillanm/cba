93e37e4a0daa6bc54937167528ed773ccb8496b8
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2022-05-11 09:19:21 +0200

    reformat code

--- src/main/java/com/elpatron/cba/CbaApplication.java
+++ src/main/java/com/elpatron/cba/CbaApplication.java
@@ -2,24 +2,11 @@ package com.elpatron.cba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class CbaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CbaApplication.class, args);
-    }
-
-    @Configuration
-    public class WebConfig extends WebMvcConfigurerAdapter {
-
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-        }
     }
 }
