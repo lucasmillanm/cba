db67fd459b1df7aa02f926cac402ee40170de13a
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2022-09-01 11:59:20 +0200

    modify authorization requests

--- src/main/java/com/elpatron/cba/security/SecurityConfig.java
+++ src/main/java/com/elpatron/cba/security/SecurityConfig.java
@@ -29,18 +29,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
-   @Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-       AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManagerBean());
-       authenticationFilter.setFilterProcessesUrl("/api/login");
-       http.csrf().disable();
-       http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-       http.authorizeRequests().antMatchers("/api/login/**", "/api/token/refresh").permitAll();
-       http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/user/**").hasAnyAuthority("USER");
-       http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user/save/**").hasAnyAuthority("ADMIN");
-       http.authorizeRequests().anyRequest().authenticated();
-       http.addFilter(authenticationFilter);
-       http.addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManagerBean());
+        authenticationFilter.setFilterProcessesUrl("/cba/login");
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers("/cba/login/**", "/cba/token/refresh").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/cba/players/**", "/cba/teams/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/cba/users/**").hasAnyAuthority("USER");
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/cba/users/**").hasAnyAuthority("ADMIN");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(authenticationFilter);
+        http.addFilterBefore(new AuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
