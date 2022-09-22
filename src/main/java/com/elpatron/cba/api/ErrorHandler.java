e555cb9c46763cd2e1dea5a5d55ef3e694ee6d22
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2022-09-22 15:57:15 +0200

    add method not allowed error

--- src/main/java/com/elpatron/cba/api/ErrorHandler.java
+++ src/main/java/com/elpatron/cba/api/ErrorHandler.java
@@ -2,6 +2,7 @@ package com.elpatron.cba.api;
 
 import com.elpatron.cba.exception.ApplicationError;
 import com.elpatron.cba.exception.BadRequestException;
+import com.elpatron.cba.exception.MethodNotAllowedException;
 import com.elpatron.cba.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,8 +15,17 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestController
 public class ErrorHandler extends ResponseEntityExceptionHandler {
 
-    public static final int NOT_FOUND = 101;
-    public static final int BAD_REQUEST = 102;
+    public static final int BAD_REQUEST = 400;
+    public static final int NOT_FOUND = 404;
+    public static final int METHOD_NOT_ALLOWED = 405;
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ApplicationError> handleBadRequestException(BadRequestException exception) {
+        ApplicationError error = new ApplicationError();
+        error.setCode(BAD_REQUEST);
+        error.setMessage(exception.getMessage());
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApplicationError> handleNotFoundException(NotFoundException exception) {
@@ -25,11 +35,11 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ApplicationError> handleBadRequestException(BadRequestException exception) {
+    @ExceptionHandler(MethodNotAllowedException.class)
+    public ResponseEntity<ApplicationError> handleMethodNotAllowedException(MethodNotAllowedException exception) {
         ApplicationError error = new ApplicationError();
-        error.setCode(BAD_REQUEST);
+        error.setCode(METHOD_NOT_ALLOWED);
         error.setMessage(exception.getMessage());
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(error, HttpStatus.METHOD_NOT_ALLOWED);
     }
 }
