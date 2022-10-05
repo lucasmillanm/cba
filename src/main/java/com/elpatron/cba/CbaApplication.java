84f80944253492f5ee987ebd494317c480499e99
lucasmillanm <70648632+lucasmillanm@users.noreply.github.com>
2022-10-05 16:42:59 +0200

    remove comment

--- src/main/java/com/elpatron/cba/CbaApplication.java
+++ src/main/java/com/elpatron/cba/CbaApplication.java
@@ -1,10 +1,5 @@
 package com.elpatron.cba;
 
-import com.elpatron.cba.model.Role;
-import com.elpatron.cba.model.User;
-import com.elpatron.cba.service.RoleService;
-import com.elpatron.cba.service.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -24,20 +19,4 @@ public class CbaApplication {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-    /*@Bean
-    CommandLineRunner run(UserService userService, RoleService roleService) {
-        return args -> {
-            userService.addNewUser(new User(null, "test", "test", "test", new ArrayList<>()));
-            userService.addNewUser(new User(null, "test2", "test2", "test2", new ArrayList<>()));
-            userService.addNewUser(new User(null, "test3", "test3", "test3", new ArrayList<>()));
-
-            roleService.addNewRole(new Role(null, "USER"));
-            roleService.addNewRole(new Role(null, "ADMIN"));
-
-            roleService.addUserRole("test" , "ADMIN");
-            roleService.addUserRole("test" , "USER");
-            roleService.addUserRole("test2" , "USER");
-        };
-    }*/
 }
